c4f486981028e553e479e256e1c54c1d230fa713
jota <jota@ubuntudesk>
2018-02-14 04:01:16 -0600

    Add basic console to Peer

--- japster/src/japster/peer/Peer.java
+++ japster/src/japster/peer/Peer.java
@@ -1,6 +1,8 @@
 package japster.peer;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -81,7 +83,23 @@ public class Peer {
             String localAddress = args[1];
             int localPort = Integer.parseInt(args[2]);
             
-            peer.search("test");
+            BufferedReader cin = new BufferedReader( new InputStreamReader(System.in));
+            String line;
+            while ( (line = cin.readLine()) != null) {
+            	switch(line) {
+            	case "search": 
+            		peer.search("test");
+            		break;
+            	case "register": 
+            		peer.updateFileRigistry();
+            		break;
+            	case "quit":
+                    System.out.println("quitting");
+            		return;
+            	}
+            	
+            }
+            
 
         } catch (Exception e) {
             System.err.println("Client exception:");

35ca3a7d0370c968186a822c896ddc8d92accb5b
jota <jota@ubuntudesk>
2018-02-18 03:22:04 -0600

    Remove unused imports

--- japster/src/japster/index/IndexServer.java
+++ japster/src/japster/index/IndexServer.java
@@ -5,9 +5,6 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.Map;
 
 import japster.common.Const;
 

3d05fe11c5d2975edb4d8d82d59553992a010c15
jota <jota@ubuntudesk>
2018-02-18 03:32:11 -0600

    Add comments to Index and IndexServer

--- japster/src/japster/index/Index.java
+++ japster/src/japster/index/Index.java
@@ -4,7 +4,26 @@ import java.net.InetSocketAddress;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * Interface exposed by IndexServer remote object. Peers use this interface to
+ * register and search for files on the IndexServer  
+ * @author jota
+ *
+ */
 public interface Index extends Remote {
+	/**
+	 * Searches the Index for a file and returns a FileLocator. 
+	 * @param name The String to be searched for. 
+	 * @return FileLocator or null if not found.
+	 * @throws RemoteException
+	 */
 	FileLocator search(String name) throws RemoteException;
+	/**
+	 * Registers a  new location for a file on the Index.
+	 * @param address InetSocketAddress containing the host address and port number
+	 * where the file can be requested
+	 * @param name String containing the name of the file that is being registered
+	 * @throws RemoteException
+	 */
 	void register(InetSocketAddress address, String name) throws RemoteException;
 }
--- japster/src/japster/index/IndexServer.java
+++ japster/src/japster/index/IndexServer.java
@@ -8,28 +8,36 @@ import java.rmi.server.UnicastRemoteObject;
 
 import japster.common.Const;
 
+/**
+ * Implements the IndexServer for the P2P application.
+ * 
+ * The IndexServer class implements the Remote interface Index, which can be used by 
+ * peers to register the location of files and to search for files on the P2P network.
+ * 
+ * The index information is kept on a FileIndex object. 
+ * @author jota
+ *
+ */
 public class IndexServer implements Index {
 
+	/**
+	 * Keeps the actual file index
+	 */
 	private FileIndex fileIndex; 
 	
 	public IndexServer() {
 		this.fileIndex = new FileIndex(); 
 	}
 	
- 	public void exportIndex() throws RemoteException { 
- 		Index stub = (Index) UnicastRemoteObject.exportObject(this, Const.INDEX_SERVICE_PORT);
-		Registry registry = LocateRegistry.createRegistry(Const.INDEX_REGISTRY_PORT);
-        registry.rebind(Const.INDEX_SERVICE_NAME, stub);
-  	}
-	
-
 	public static void main(String[] args) {
 		try {
 			IndexServer server = new IndexServer();
 			
+			//Initialize RMI
 			server.exportIndex();
 			
 	        System.out.println("Index server started");
+
 		} catch (Exception e) { 
             System.err.println("Cant start index server:");
             e.printStackTrace();
@@ -53,4 +61,15 @@ public class IndexServer implements Index {
 		FileLocation location = new FileLocation(address);
 		fileIndex.register(name, location);
 	}
+
+ 	/**
+ 	 * Exports the remote object, creates a registry and binds the remote object 
+ 	 * to the registry
+ 	 * @throws RemoteException
+ 	 */
+ 	private void exportIndex() throws RemoteException { 
+ 		Index stub = (Index) UnicastRemoteObject.exportObject(this, Const.INDEX_SERVICE_PORT);
+		Registry registry = LocateRegistry.createRegistry(Const.INDEX_REGISTRY_PORT);
+        registry.rebind(Const.INDEX_SERVICE_NAME, stub);
+  	}
 }

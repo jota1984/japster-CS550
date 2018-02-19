c8ecf7a279c63992bddd5005264e091ffd577ea9
jota <jota@ubuntudesk>
2018-02-18 18:27:49 -0600

    Add comments to FileWatcher class

--- japster/src/japster/index/FileWatcher.java
+++ japster/src/japster/index/FileWatcher.java
@@ -2,6 +2,13 @@ package japster.index;
 
 import java.util.Date;
 
+/**
+ * A FileWatcher thread is attached to a FileIndex and periodically calls the purge 
+ * method on it to make sure that files that have not been refreshed for a while are removed 
+ * from the index.
+ * @author jota
+ *
+ */
 public class FileWatcher extends Thread{
 	private static final int WATCH_PERIOD = 10000;
 	private FileIndex fileIndex;
